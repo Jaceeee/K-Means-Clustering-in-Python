32f2e399da0dafae0328b652c7416406e8676d2f
Jace Roldan <jtroldan@up.edu.ph>
2017-12-03 14:24:48 +0800

    Write cost function

--- mp2.py
+++ mp2.py
@@ -3,7 +3,8 @@ import math
 import statistics
 # constructing the array using list comprehensions
 # file_path = '/home/jaceroldan/Documents/3rd Year/Introduction to Artificial Intelligence/Machine Problems/2/Machine Problem 2/kmdata1.txt'
-file_path = 'kmdata1.txt'
+file_path = './files/kmdata1.txt'
+output_file_paths = './output/'
 
 file = open(file_path, 'r')
 
@@ -39,8 +40,11 @@ for i in range(0, k):
 # print(centroid)
 
 #stores all the differences in one 2D array
-n = int(input("Enter a number: "))
+inp = int(input("Enter a number: "))
+n = inp
+cost = 0
 while n > 0:
+	dj = cost
 	differences_array = []
 
 	for i in range(0, len(vals)):
@@ -59,17 +63,22 @@ while n > 0:
 	assignment = []
 	for i in range(0, len(vals)):		
 		assignment.append(differences_array[i].index(min(differences_array[i])))
-		differences_array[i] = [min(differences_array[i])]	
-	print(assignment)
+		differences_array[i] = min(differences_array[i])
+	
+	# print(assignment)
+	output_file_ca = open(output_file_paths+'iter'+str(10-n+1)+'_ca.txt', 'w+')	
+	to_file_output_assignment = ''.join((str(e)+'\n') for e in assignment)	
+	# print(output_assignment)
+	output_file_ca.write(to_file_output_assignment)
+	output_file_ca.close()
 
-	# TODO change the centroids
 	means_array = []
 	for i in range(0, k):
 		means_array.append([])
 
 	# change centroid step
 	print("iteration ", 10-n+1, ":", sep="")
-	print("before... ", centroid)
+	# print("before... ", centroid)
 	for i in range(0, k):			#for each centroid value		    
 	    for j in range(0, features): #going through each feature
 	        sum_arr = []  #set a sum array	        
@@ -80,5 +89,25 @@ while n > 0:
 	        if len(sum_arr) > 0:
 	        	centroid[i][j] = statistics.mean(sum_arr)
 
-	print("after...  ", centroid)
+	# print("after...  ", centroid)
+	#TODO insert file writing of centroid locations into "iter<n>_cm.txt"	
+	output_file_cm = open(output_file_paths+'iter'+str(10-n+1)+'_cm.txt', 'w+')
+	to_file_output_centroid = ''
+
+	for i in range(0, len(centroid)):
+	    to_file_output_centroid = to_file_output_centroid + '(' + ''.join((str(e) + ', ') for e in centroid[i]) + '\b\b)' + '\n'	    
+	    # print(centroid[i])	    
+
+
+	# to_file_output_centroid = ''.join(((str(e) + ' ') for e in ordered_pair) for ordered_pair in centroid)
+	#TODO insert file appending J, value of distortion function, into "iter<n>_cm.txt"
+	#TODO insert file appending dJ, value of the difference of the current J from the previous J, into "iter<n>_cm.txt"	
+
+	cost =  statistics.mean(differences_array)	
+	dj = 0 if n == inp else dj - cost
+	
+	to_file_output_centroid = to_file_output_centroid + str(cost) + '\n' + str(dj)
+
+	print(to_file_output_centroid)
+	output_file_cm.close()
 	n -= 1
